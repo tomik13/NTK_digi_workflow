91f6db462368dd2cd10662a7859bc4a370dcba77
Tomáš Lysoněk <lysonekt@localhost.localdomain>
2024-04-05 14:24:23 +0200

    making order

--- config_example.py
+++ config_example.py
@@ -1,15 +1,15 @@
 import os
 
 
-workflow_home = "/mnt/moon/DIGITALIZACE/WORKFLOW_TEST/"
+workflow_home = ""
 
 NDK_input_directory = os.path.join(workflow_home,'new_exports')
 NDK_loaded_directory = os.path.join(workflow_home,'loaded')
 
-data_directory ='/mnt/moon/DIGITALIZACE/WORKFLOW_TEST/data'
+data_directory =''
 
 aleph_lines_directory = ''
 
-default_archive = '/mnt/moon/DIGITALIZACE/WORKFLOW_TEST/to_archive'
+default_archive = ''
 
 scan_input_directory = os.path.join(workflow_home,'new_scans')
