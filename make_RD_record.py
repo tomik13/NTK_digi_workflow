799e60dac27984e3c96e98ed2cda7f7eff01921c
Tomáš Lysoněk <tomas.lysonek@techlib.cz>
2024-04-22 16:59:56 +0200

    minor corrections

--- make_RD_record.py
+++ make_RD_record.py
@@ -5,16 +5,16 @@ stylesheet = e.parse('utils/rd_export_tl.xsl')
 transform = e.XSLT(stylesheet)
 
 def make_RD_record(digi_info):
-    recId = digi_info['001'] if '001' in digi_info else digi_info['sysno']
+    recId = str(digi_info['001'] if '001' in digi_info else digi_info['sysno'])
     ne = aleph_oai_marcSLIM(digi_info['sysno'])
-    nr = transform(ne,FMT=f"'{digi_info['FMT']}'",recId = recId)
+    nr = transform(ne,FMT=f"'{digi_info['FMT']}'",recId = f"'{recId}'")
     return nr
 
 def make_RD_collection(control_list,filename):
-    newBatch = e.parse('hlaseniRD/empty.xml')
+    newBatch = e.parse('utils/empty.xml')
     root = newBatch.getroot()
-    for urn in control_list:
-        digi_info = control_list[urn]
+    for digi_info in control_list:
+        ## digi_info = control_list[urn]
         nr = make_RD_record(digi_info)
         root.append(nr.getroot())
         digi_info['RD_batch_file'] = filename
